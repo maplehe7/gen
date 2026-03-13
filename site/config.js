a9650b7f2546fd48d0fdc3eac8ccad7fc54895a9
maplehe7 <144498852+maplehe7@users.noreply.github.com>
2026-03-12 18:50:49 -0700

    Update config.js

--- site/config.js
+++ site/config.js
@@ -1,6 +1,6 @@
 window.STANDALONE_FORGE_CONFIG = {
-  owner: "",
-  repo: "",
+  owner: "maplehe7",
+  repo: "gen",
   ref: "main",
-  token: "PASTE_FINE_GRAINED_PAT_HERE",
+  token: "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",
 };
